27b232b125253aae9bccc50d0f272d059bfe108a
rajeshbj <31621043+rajeshbj@users.noreply.github.com>
2017-09-18 15:06:40 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/ChatUsers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsoleApp1
+{
+    public class ChatUsers
+    {
+        public virtual int Id { get; set; }
+        public virtual string Userid { get; set; }
+        public virtual string password { get; set; }
+        public virtual string Status { get; set; }
+        public virtual string Username { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new Configuration();
+            String connstring = "Server=119.82.106.246;userid=postgres;pwd=Stixis@123; database=SingnalRChatDb";
+
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = connstring;
+                x.Driver<NpgsqlDriver>();
+                x.Dialect<PostgreSQLDialect>();
+            });
+
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            var sefact = cfg.BuildSessionFactory();
+            using (var session = sefact.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    var users = session.CreateCriteria<ChatUsers>().List<ChatUsers>();
+                    foreach(var user in users)
+                    {
+                        Console.WriteLine("{0} \t{1} \t{2}", user.Id, user.Userid, user.Username);
+                    }
+                    tx.Commit();
+                }
+                Console.ReadLine();
+            }
+           
+
+        }
+    }
+}
